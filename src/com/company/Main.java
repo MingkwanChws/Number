d9b8141280bdb4d1c4e07589549e82f8bc37127a
MingkwanChws <58773342+MingkwanChws@users.noreply.github.com>
2019-12-12 23:11:56 +0700

    Add Scanner in main()

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,16 @@
 package com.company;
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
         int a;
         int b;
         int result;
+        Scanner number = new Scanner(System.in);
+        System.out.println("enter a");
+        a = number.nextInt();
+        System.out.println("enter b");
+        b = number.nextInt();
+
     }
 }
